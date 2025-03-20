5fd160161360f27f3bdacb156eac14d965214185
Igor Safonov <safonoff@gmail.com>
2025-03-20 11:37:53 +0200

    Updated space subdivision to work with glMatrix vectors

--- js/space_subdiv.js
+++ js/space_subdiv.js
@@ -1,118 +1,90 @@
 class SubDiv {
-	constructor(w, h, s) {
-		this.size = s;
-		this.buckets = [];
-		this.rowLength = Math.ceil(w / s);
-		this.count = this.rowLength * Math.ceil(h / s);
+	constructor(width, height, bucketSize) {
+		this.width = width;
+		this.height = height;
+		this.bucketSize = bucketSize;
+		this.bucketSizeInv = 1 / bucketSize;
 
-		this.grids = [];
+		this.cols = Math.ceil(width * this.bucketSizeInv);
+		this.rows = Math.ceil(height * this.bucketSizeInv);
 
-		for (let i = 0; i < this.count; i++) {
-			this.buckets.push([]);
+		this.buckets = new Array(this.cols * this.rows);
+		for (let i = 0; i < this.buckets.length; i++) {
+			this.buckets[i] = [];
 		}
 	}
 
 	update(item) {
-		const i = Math.floor(item.position.y / this.size) * this.rowLength + Math.floor(item.position.x / this.size);
-		if (i !== item.bucket) {
-			if (item.bucket !== undefined) {
-				const oldB = this.buckets[item.bucket];
-				const ii = item.bucket_index;//oldB.indexOf(item);
-				// Remove from the old bucket
-				oldB[ii] = oldB[oldB.length - 1]; // It will be replaced with the last item.
-				oldB[ii].bucket_index = ii; // update the index.
-				oldB.pop(); // And the last item will be removed.
+		const x = item.position[0];
+		const y = item.position[1];
+
+		const col = Math.floor(x * this.bucketSizeInv);
+		const row = Math.floor(y * this.bucketSizeInv);
+
+		if (item.bucketIndex !== undefined) {
+			const oldBucket = this.buckets[item.bucketIndex];
+			const index = oldBucket.indexOf(item);
+			if (index > -1) {
+				oldBucket.splice(index, 1);
 			}
-			item.bucket = i;
-			item.bucket_index = this.buckets[i].length;
-			this.buckets[i].push(item);
 		}
+
+		const bucketIndex = row * this.cols + col;
+		this.buckets[bucketIndex].push(item);
+		item.bucketIndex = bucketIndex;
 	}
 
-	getNearItems(item, dbg = false) {
-		this.debug = dbg;
+	getNearItems(item) {
+		const x = item.position[0];
+		const y = item.position[1];
 
-		let all = [];
-		// Item is in the center, get the top left corner from the 3x3
-		let y = Math.floor(item.bucket / this.rowLength);
-		let x = item.bucket - y * this.rowLength;
-		let i = item.bucket - 1 - this.rowLength;
-		x--;
-		y--;
-		x *= this.size;
-		y *= this.size;
+		const col = Math.floor(x * this.bucketSizeInv);
+		const row = Math.floor(y * this.bucketSizeInv);
 
-		/**
-		 * 00 01 02
-		 * X0 X1 X2
-		 * Y0 Y1 Y2
-		 */
-		this.concat(all, i, x, y); // 00
-		i++;
-		x += this.size;
-		this.concat(all, i, x, y); // 01
-		i++;
-		x += this.size;
-		this.concat(all, i, x, y); // 02
-		i += this.rowLength - 2;
-		x -= this.size + this.size;
-		y += this.size;
+		const nearBuckets = [];
 
-		this.concat(all, i, x, y); // X0
-		i++;
-		x += this.size;
-		this.concat(all, i, x, y); // X1
-		i++;
-		x += this.size;
-		this.concat(all, i, x, y); // X2
-		i += this.rowLength - 2;
-		x -= this.size + this.size;
-		y += this.size;
-
-		this.concat(all, i, x, y); // Y0
-		i++;
-		x += this.size;
-		this.concat(all, i, x, y); // Y1
-		i++;
-		x += this.size;
-		this.concat(all, i, x, y); // Y2
-
-		return all;
-	}
-
-	concat(all, i, x, y) {
-		if (x >= 0 && y >= 0 && x < width && y < height) { 			
-			all.push(this.buckets[i]); // collect the buckets onnly and iterate on them later - should be faster
-
-			if (this.debug) this.addDebugSquare(x, y);
+		for (let i = -1; i <= 1; i++) {
+			const r = row + i;
+			if (r >= 0 && r < this.rows) {
+				for (let j = -1; j <= 1; j++) {
+					const c = col + j;
+					if (c >= 0 && c < this.cols) {
+						const bucket = this.buckets[r * this.cols + c];
+						if (bucket.length > 0) {
+							nearBuckets.push(bucket);
+						}
+					}
+				}
+			}
 		}
+
+		return nearBuckets;
 	}
 
 	debugGrid() {
-		if (this.visual === undefined) {
-			this.visual = new PIXI.Graphics();
-			app.stage.addChild(this.visual);
-			
-			this.visual.clear();
+		if (!DEBUG) return;
+
+		const x = flock[0].position[0];
+		const y = flock[0].position[1];
+
+		const col = Math.floor(x * this.bucketSizeInv);
+		const row = Math.floor(y * this.bucketSizeInv);
+
+		for (let i = -1; i <= 1; i++) {
+			const r = row + i;
+			if (r >= 0 && r < this.rows) {
+				for (let j = -1; j <= 1; j++) {
+					const c = col + j;
+					if (c >= 0 && c < this.cols) {
+						const bucket = this.buckets[r * this.cols + c];
+						if (bucket.length > 0) {
+							bucket.forEach(a => {
+								a.isNear = true;
+							});
+						}
+					}
+				}
+			}
 		}
-
-		this.grids.forEach(g => g.visible = false);
-		this.gridIndex = 0;
-
-		this.visual.visible = DEBUG;
-	}
-
-	addDebugSquare(x, y) {
-		if (this.grids[this.gridIndex] === undefined) {
-			const g = this.grids[this.gridIndex] = new PIXI.Graphics();
-			app.stage.addChild(g);
-
-
-		}
-
-		this.grids[this.gridIndex].x = x;
-		this.grids[this.gridIndex].y = y;
-		this.grids[this.gridIndex].visible = true;
-		this.gridIndex++;
 	}
 }
